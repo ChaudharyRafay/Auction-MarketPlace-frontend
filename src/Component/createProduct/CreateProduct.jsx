c3435f53e2e00de41c6461f6d941fb66402cec1f
pakCoderboy <118123660+pakCoderboy@users.noreply.github.com>
2023-06-01 08:07:21 +0500

    create product update

--- src/Component/createProduct/CreateProduct.jsx
+++ src/Component/createProduct/CreateProduct.jsx
@@ -22,8 +22,16 @@ const CreateProduct = () => {
     price: "",
     image: null,
   });
+  const [errors, setErrors] = useState({});
+
   const formData = new FormData();
   const handleSubmit = async () => {
+    // Validate the form fields
+    console.log("-----");
+    const formIsValid = validateForm();
+    if (!formIsValid) {
+      return;
+    }
     formData.append("itemName", data.itemName);
     formData.append("description", data.description);
     formData.append("property1", data.property1);
@@ -47,6 +55,53 @@ const CreateProduct = () => {
       console.log(error.stack);
     }
   };
+  const validateForm = () => {
+    let isValid = true;
+    const errorsCopy = {};
+
+    if (data.itemName.trim() === "") {
+      errorsCopy.itemName = "*Item name is required field";
+      isValid = false;
+    }
+
+    if (data.description.trim() === "") {
+      errorsCopy.description = "*Description is required field";
+      isValid = false;
+    }
+    if (data.property1.trim() === "") {
+      errorsCopy.property1 = "*Property1 is required field";
+      isValid = false;
+    }
+    if (data.property2.trim() === "") {
+      errorsCopy.property2 = "*Property2 is required field";
+      isValid = false;
+    }
+    if (data.property3.trim() === "") {
+      errorsCopy.property3 = "Property3 is required field";
+      isValid = false;
+    }
+    if (data.startDate.trim() === "") {
+      errorsCopy.startDate = "*Start Date is required field";
+      isValid = false;
+    }
+    if (data.endDate.trim() === "") {
+      errorsCopy.endDate = "*End Date is required field";
+      isValid = false;
+    }
+    if (data.price.trim() === "") {
+      errorsCopy.price = "*Price is required field";
+      isValid = false;
+    }
+    if (data.image === null) {
+      errorsCopy.image = "*Image is required field";
+      isValid = false;
+    }
+
+    setErrors(errorsCopy);
+    return isValid;
+  };
+  const [selectedImage, setSelectedImage] = useState(null);
+  console.log(selectedImage);
   return (
     <div>
       <section className="create-product ">
@@ -64,26 +119,92 @@ const CreateProduct = () => {
             <div className="left">
               <h5>upload file</h5>
               <h6>Drag or Choose your file to upload</h6>
-              <div className="upload-file">
-                <p>
-                  PNG, GIF, WEBP, MP4 or MP3 . <br /> Max 100mb
-                </p>
-                <div className="uploader">
-                  <label htmlFor="file">Upload File</label>
-                  <input
-                    type="file"
-                    name=""
-                    id="file"
-                    required
-                    onChange={(e) => {
-                      setdata((prevState) => ({
-                        ...prevState,
-                        image: e.target.files[0],
-                      }));
-                    }}
-                  />
+              {!selectedImage ? (
+                <>
+                  <div className="upload-file">
+                    <p>
+                      PNG, GIF, WEBP, MP4 or MP3 . <br /> Max 100mb
+                    </p>
+                    <div className="uploader">
+                      <label htmlFor="file">Upload File</label>
+                      <input
+                        type="file"
+                        name=""
+                        id="file"
+                        required
+                        onChange={(e) => {
+                          setdata((prevState) => ({
+                            ...prevState,
+                            image: e.target.files[0],
+                          }));
+                          setSelectedImage(
+                            URL.createObjectURL(e.target.files[0])
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
+                  {!selectedImage && errors.image && (
+                    <span className="error text-danger">{errors.image}</span>
+                  )}
+                </>
+              ) : (
+                <div>
+                  <div
+                    class="mb-4  d-flex flex-column justify-content-center"
+                    // style={{ border: "3px dashed #000000" }}
+                  >
+                    <img
+                      src={selectedImage}
+                      alt="example placeholder"
+                      style={{ width: "300px" }}
+                    />
+                  </div>
+
+                  <div class="d-flex justify-content-center">
+                    <div class="btn  btn-rounded">
+                      <label
+                        class="form-label text-white m-1"
+                        for="customFile1"
+                        style={{
+                          fontFamily: "Trap600",
+                          fontStyle: "normal",
+                          fontWeight: "600",
+                          fontSize: "12px",
+                          lineHeight: "48px",
+                          letterSpacing: "1px",
+                          textTransform: "uppercase",
+                          color: "#ffffff",
+                          background: "linear-gradient(0deg, #000000, #000000)",
+                          boxShadow: "0px 3px 20px rgba(0, 0, 0, 0.1)",
+                          borderRadius: " 10px",
+                          width: "160px",
+                          cursor: "pointer",
+                        }}
+                      >
+                        Choose file
+                      </label>
+
+                      <input
+                        type="file"
+                        class="form-control d-none"
+                        name=""
+                        id="customFile1"
+                        required
+                        onChange={(e) => {
+                          setdata((prevState) => ({
+                            ...prevState,
+                            image: e.target.files[0],
+                          }));
+                          setSelectedImage(
+                            URL.createObjectURL(e.target.files[0])
+                          );
+                        }}
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
             <div className="right">
               <div className="product-data">
@@ -97,8 +218,12 @@ const CreateProduct = () => {
                         ...prevState,
                         itemName: e.target.value,
                       }));
+                      e.target.value !== 0 ? (errors.itemName = "") : null;
                     }}
                   />
+                  {errors.itemName && (
+                    <span className="error text-danger">{errors.itemName}</span>
+                  )}
 
                   <p className="mt-3">Description</p>
                   <textarea
@@ -111,8 +236,14 @@ const CreateProduct = () => {
                         ...prevState,
                         description: e.target.value,
                       }));
+                      e.target.value !== 0 ? (errors.description = "") : null;
                     }}
                   />
+                  {errors.description && (
+                    <span className="error text-danger">
+                      {errors.description}
+                    </span>
+                  )}
                 </div>
                 <div className="row mt-3 product-entries custom-select">
                   <div className="col-md-4 col-12">
@@ -125,8 +256,14 @@ const CreateProduct = () => {
                           ...prevState,
                           property1: e.target.value,
                         }));
+                        e.target.value !== 0 ? (errors.property1 = "") : null;
                       }}
                     />
+                    {errors.property1 && (
+                      <span className="error text-danger">
+                        {errors.property1}
+                      </span>
+                    )}
                   </div>
                   <div className="col-md-4 col-12">
                     <p>Property 2</p>
@@ -138,8 +275,14 @@ const CreateProduct = () => {
                           ...prevState,
                           property2: e.target.value,
                         }));
+                        e.target.value !== 0 ? (errors.property2 = "") : null;
                       }}
                     />
+                    {errors.property2 && (
+                      <span className="error text-danger">
+                        {errors.property2}
+                      </span>
+                    )}
                   </div>
                   <div className="col-md-4 col-12">
                     <p>Property 3 </p>
@@ -151,8 +294,14 @@ const CreateProduct = () => {
                           ...prevState,
                           property3: e.target.value,
                         }));
+                        e.target.value !== 0 ? (errors.property3 = "") : null;
                       }}
                     />
+                    {errors.property3 && (
+                      <span className="error text-danger">
+                        {errors.property3}
+                      </span>
+                    )}
                   </div>
                 </div>
 
@@ -170,8 +319,14 @@ const CreateProduct = () => {
                             .utc(e.target.value)
                             .format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
                         }));
+                        e.target.value !== 0 ? (errors.startDate = "") : null;
                       }}
                     />
+                    {errors.startDate && (
+                      <span className="error text-danger">
+                        {errors.startDate}
+                      </span>
+                    )}
                   </div>
                   <div className="col-md-4 col-12">
                     <p>End Date</p>
@@ -186,8 +341,14 @@ const CreateProduct = () => {
                             .utc(e.target.value)
                             .format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
                         }));
+                        e.target.value !== 0 ? (errors.endDate = "") : null;
                       }}
                     />
+                    {errors.endDate && (
+                      <span className="error text-danger">
+                        {errors.endDate}
+                      </span>
+                    )}
                   </div>
                   <div className="col-md-4 col-12">
                     <p>Price</p>
@@ -200,8 +361,12 @@ const CreateProduct = () => {
                           ...prevState,
                           price: e.target.value,
                         }));
+                        e.target.value !== 0 ? (errors.price = "") : null;
                       }}
                     />
+                    {errors.price && (
+                      <span className="error text-danger">{errors.price}</span>
+                    )}
                   </div>
                 </div>
                 <button onClick={handleSubmit}>create Product</button>
